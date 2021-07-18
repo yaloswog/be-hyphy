66518a4a8a9d098e4df59156d8481f1725c55c5b
yaloswog <yaloswog@gmail.com>
2021-07-18 16:54:22 -0300

    color changer v1

---
+++ public/main.js
@@ -0,0 +1,14 @@
+function blueMode() {
+  var element = document.body;
+  element.classList.toggle("blue-mode");
+}
+
+function redMode() {
+    var element = document.body;
+    element.classList.toggle("red-mode");
+  }
+
+  function grayMode() {
+    var element = document.body;
+    element.classList.toggle("gray-mode");
+  }
